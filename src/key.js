ebc9d8bc0c2afbaed459ad581ae5821891e09a6d
Rohit8412 <rohitnshilpa@gmail.com>
2023-08-10 12:57:05 +0530

    last commit

--- src/key.js
+++ src/key.js
@@ -1,10 +1,10 @@
 export const API_URL = window.location.hostname.includes("localhost")
   ? "http://localhost:4000"
-  : "https://poised-yak-hose.cyclic.app";
+  : "https://quaint-duck-sweatshirt.cyclic.app";
 
 export const CLIENT_URL = window.location.hostname.includes("localhost")
   ? "http://localhost:3000"
-  : "https://zomato-cl.netlify.app/delivery";
+  : "https://lama-food-delivery.netlify.app/delivery";
 
 // module.exports = API_URL;
 // module.exports = CLIENT_URL;
